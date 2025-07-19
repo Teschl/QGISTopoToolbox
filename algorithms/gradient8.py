45f1396e990fc763c252ebf1f1a06c26baeaf32e
Theophil Bringezu <69400012+Teschl@users.noreply.github.com>
2025-07-19 16:02:22 +0200

    Add gradient8

---
+++ algorithms/gradient8.py
@@ -0,0 +1,99 @@
+import os
+
+# pylint: disable=import-error
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessingAlgorithm,
+                       QgsProcessingParameterRasterLayer,
+                       QgsProcessingParameterBoolean,
+                       QgsProcessingParameterEnum,
+                       QgsProcessingParameterRasterDestination,
+                       QgsProcessingException)
+from qgis.PyQt.QtGui import QIcon
+
+# Import topotoolbox, has to be installed in OSGeo4W Shell (Windows)
+# 1. Open OSGeo4W Shell
+# 2. Run: pip install topotoolbox
+# Not jet working on Linux (and MacOS) due to missing OSGeo4W Shell
+import topotoolbox as tt
+
+class Gradient8(QgsProcessingAlgorithm):
+    INPUT_RASTER = 'INPUT_RASTER'
+    UNIT = 'UNIT'
+    MULTIPROCESSING = 'MULTIPROCESSING'
+    OUTPUT = 'OUTPUT'
+
+    def createInstance(self):
+        return Gradient8()
+
+    def tr(self, string):
+        return QCoreApplication.translate('Processing', string)
+
+    def name(self):
+        return 'gradient8'
+
+    def displayName(self):
+        return self.tr('Gradient8')
+    
+    def shortHelpString(self):
+        return self.tr("")
+
+    def icon(self):
+        return QIcon(os.path.join(os.path.dirname(__file__), 'icons', 'gradient8.png'))
+
+    def initAlgorithm(self, config=None):
+        self.addParameter(
+            QgsProcessingParameterRasterLayer(
+                self.INPUT_RASTER,
+                self.tr('Input DEM')
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterEnum(
+                self.UNIT,
+                'Calculation method',
+                options=['tangent', 'radians', 'degrees', 'sine', 'percent'],
+                defaultValue='tangent',
+                optional=False
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterBoolean(
+                self.MULTIPROCESSING,
+                self.tr('Threshold for excess topography calculation'),
+                defaultValue=True
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterRasterDestination(
+                self.OUTPUT,
+                self.tr('')
+            )
+        )
+
+    def processAlgorithm(self, parameters, context, feedback):
+        input_raster = self.parameterAsRasterLayer(parameters, self.INPUT_RASTER, context)
+        multiprocessing = self.parameterAsBoolean(parameters, self.MULTIPROCESSING, context)
+        unit = self.parameterAsEnum(parameters, self.UNIT, context)
+
+        if input_raster is None:
+            raise QgsProcessingException("Invalid input raster layer")
+
+        input_path = input_raster.source()
+        dem = tt.read_tif(input_path)
+
+        if unit == 0:
+            unit = 'tsngent'
+        elif unit == 1:
+            unit = 'radians'
+        elif unit == 2:
+            unit = 'degrees'
+        elif unit == 3:
+            unit = 'sine'
+        else:
+            unit = 'percent'
+
+        result_dem = dem.gradient8(unit=unit, multiprocessing=multiprocessing)
+
+        output_path = self.parameterAsOutputLayer(parameters, self.OUTPUT, context)
+        tt.write_tif(result_dem, output_path)
+        return {self.OUTPUT: output_path}
