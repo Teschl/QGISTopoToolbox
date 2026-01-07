56b1f1f4d51c3de4314489b3336f64e99d7abeb9
Teschl <69400012+Teschl@users.noreply.github.com>
2026-01-07 19:14:26 +0100

    switch to ruff and black

--- __init__.py
+++ __init__.py
@@ -1,25 +1,15 @@
 def classFactory(iface):
     """Load TopoToolbox plugin."""
-    
+
     # Check if topotoolbox package is installed and abort loading if not
     from qgis.PyQt.QtWidgets import QMessageBox
+
     try:
         __import__("topotoolbox")
     except ImportError:
-        QMessageBox.warning(
-            None,
-            "Missing Python Package: 'topotoolbox'"
-        )
-        # returning None causes Error in QGIS but loading dummy plugin 
-        # installs a placeholder instead of aborting installation. This 
-        # placeholder has to be removed manually later.
-        # TODO: decide which is better option
-        class DummyPlugin:
-            def initGui(self): pass
-            def unload(self): pass
-            
-        # return None
-        return DummyPlugin()
-        
+        QMessageBox.warning(None, "Missing Python Package: 'topotoolbox'")
+        return None
+
     from .topotoolboxplugin import TopoToolboxPlugin
+
     return TopoToolboxPlugin(iface)
